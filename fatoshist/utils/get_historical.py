806c8518af931bab283048d02cfc63cbfe31594f
leviobrabo <carlosjunior20313@gmail.com>
2024-09-12 21:58:31 -0300

    ✨ | Subindo a coleta de dados historicos

---
+++ fatoshist/utils/get_historical.py
@@ -0,0 +1,22 @@
+import json
+from datetime import datetime
+
+from ..loggers import logger
+
+
+def get_historical_events():
+    today = datetime.now()
+    day = today.day
+    month = today.month
+    try:
+        with open('./fatoshistoricos/data/eventos.json', 'r', encoding='utf-8') as file:
+            json_events = json.load(file)
+            events = json_events[f'{month}-{day}']
+            if events:
+                return '\n\n'.join(events)
+            else:
+                return None
+    except Exception as e:
+        logger.error('Error reading events from JSON:', str(e))
+
+        return None
