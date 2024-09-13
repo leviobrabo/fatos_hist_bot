acfd0d33418c42ea4da09378aad7187cdaf3cc28
leviobrabo <carlosjunior20313@gmail.com>
2024-09-12 21:59:49 -0300

    ✨ | Subindo  sistema que verifica se o usuario é sudo

---
+++ fatoshist/utils/sudo.py
@@ -0,0 +1,9 @@
+from ..database.users import UserManager
+
+
+def sudo(user_id):
+    user_manager = UserManager()
+    user = user_manager.search_user(user_id)
+    if user and user.get('sudo') == 'true':
+        return True
+    return False
