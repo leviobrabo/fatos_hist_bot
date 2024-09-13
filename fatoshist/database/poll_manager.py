2ef4c305f28728fdd791b43f1fd85aa9202827f2
leviobrabo <carlosjunior20313@gmail.com>
2024-09-12 21:52:42 -0300

    ✨ | Adicionando classe para armazenar as enquetes no banco de dados

---
+++ fatoshist/database/poll_manager.py
@@ -0,0 +1,21 @@
+from ..database.db_connection import DBConnection
+
+
+class PollManager:
+    def __init__(self):
+        self.db_connection = DBConnection()
+        self.db = self.db_connection.get_db()
+
+    def add_poll(self, chat_id, poll_id, correct_option_id, date):
+        return self.db.poll.insert_one({
+            'chat_id': chat_id,
+            'poll_id': poll_id,
+            'correct_option_id': correct_option_id,
+            'date': date,
+        })
+
+    def search_poll(self, poll_id):
+        return self.db.poll.find_one({'poll_id': poll_id})
+
+    def remove_all_polls(self):
+        return self.db.poll.delete_many({})
