f5f5e0fdeb5b937b336810378f19a9cbc5ed1863
leviobrabo <carlosjunior20313@gmail.com>
2024-09-12 21:58:00 -0300

    ✨ | Subindo o anti spam

---
+++ fatoshist/utils/anti_spam.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+from telebot.apihelper import ApiTelegramException
+
+
+def handle_anti_spam(bot):
+    for _ in range(4):  # Tentar até 4 vezes
+        try:
+            bot.send_message()  # Tenta enviar a mensagem
+            return
+        except ApiTelegramException as ex:
+            if 'error_code' in ex.result_json and ex.result_json['error_code'] == '429':
+                retry_after = ex.result_json['parameters']['retry_after']
+                print(f'Recebido 429, esperando {retry_after} segundos.')
+                sleep(retry_after)
+            else:
+                raise
+    bot.send_message()
