13cd1c81bac0f2ec7a5fd21a30b49c9e75de91ee
leviobrabo <carlosjunior20313@gmail.com>
2024-09-12 21:49:23 -0300

    ✨ | Adicionando o registro do bot

---
+++ fatoshist/loggers.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import sys
+from logging.handlers import RotatingFileHandler
+
+from .config import LOG_PATH
+
+# Crie o diretório do arquivo de log, se não existir
+log_directory = os.path.dirname(LOG_PATH)
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+# Configura o logger com o arquivo de log
+handler = RotatingFileHandler(LOG_PATH, maxBytes=5000000, backupCount=5)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+
+logger = logging.getLogger('fatoshistbot')
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+logging.basicConfig(stream=sys.stdout, encoding='utf-8')
