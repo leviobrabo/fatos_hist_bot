2fbbd38a8bc4217ec47a8be79ee81e2700c8e205
leviobrabo <carlosjunior20313@gmail.com>
2024-09-12 21:59:26 -0300

    ✨ | Subindo sistema de nomeação dos meses

---
+++ fatoshist/utils/month.py
@@ -0,0 +1,17 @@
+def get_month_name(month):
+    month_names = [
+        '',
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro',
+    ]
+    return month_names[month]
