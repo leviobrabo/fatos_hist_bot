4a7e8220e802b62950cea4ffd22f00e85db5010d
leviobrabo <carlosjunior20313@gmail.com>
2024-09-14 19:01:59 -0300

    ✨ | Adicionando verificação por banco de dados de link para ver se evento não já foi postado

---
+++ fatoshist/database/imgs.py
@@ -0,0 +1,16 @@
+from ..database.db_connection import DBConnection
+
+
+class PhotoManager:
+    """Classe responsável por gerenciar a coleção de imagens no banco de dados."""
+
+    def __init__(self):
+        """Inicializa a conexão com o banco de dados usando DBConnection."""
+        self.db_connection = DBConnection()
+        self.db = self.db_connection.get_db()
+
+    def add_url_photo(self, photo_url):
+        return self.db.cphoto.insert_one({'photo_url': photo_url})
+
+    def remove_all_url_photo(self):
+        return self.db.cphoto.delete_many({})
