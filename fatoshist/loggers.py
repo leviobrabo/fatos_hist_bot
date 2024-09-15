729be491fb155c995cc0282929df1d483a9c495d
leviobrabo <carlosjunior20313@gmail.com>
2024-09-15 11:38:54 -0300

    ✨ | Corrigindo com ruff

--- fatoshist/loggers.py
+++ fatoshist/loggers.py
@@ -5,12 +5,10 @@ from logging.handlers import RotatingFileHandler
 
 from .config import LOG_PATH
 
-# Crie o diretório do arquivo de log, se não existir
 log_directory = os.path.dirname(LOG_PATH)
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 
-# Configura o logger com o arquivo de log
 handler = RotatingFileHandler(LOG_PATH, maxBytes=5000000, backupCount=5)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
