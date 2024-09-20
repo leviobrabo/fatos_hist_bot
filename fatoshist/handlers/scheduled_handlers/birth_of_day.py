488ba1decea86440ff5ffeecde2fa43560af99f4
Levi <115052769+leviobrabo@users.noreply.github.com>
2024-09-20 20:22:40 -0300

    Update birth_of_day.py

--- fatoshist/handlers/scheduled_handlers/birth_of_day.py
+++ fatoshist/handlers/scheduled_handlers/birth_of_day.py
@@ -19,7 +19,7 @@ def get_births_of_the_day(bot, CHANNEL):
             headers={'accept': 'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/onthisday/0.3.3"'},
         )
 
-        if response.status_code == '200':
+        if response.status_code == 200:
             data = response.json()
             births = data.get('births', [])
 
