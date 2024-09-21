dcb33df265d345493665107cc3cefd78c70fb404
leviobrabo <carlosjunior20313@gmail.com>
2024-09-21 10:08:04 -0300

    ✨ | Corrigindo o is_sudo para melhora na busca

--- fatoshist/database/users.py
+++ fatoshist/database/users.py
@@ -65,11 +65,9 @@ class UserManager:
         return self.db.users.update_one({'user_id': user_id}, {'$set': {'sudo': 'true'}})
 
     def is_sudo(self, user_id):
-        user_manager = UserManager()
-        user = user_manager.get_user(user_id)
-        if user and user.get('sudo') == 'true':
-            return True
-        return False
+        user = self.get_user(user_id)
+        return user is not None and user.get('sudo') == 'true'
+
 
     def remove_user_sudo(self, user_id):
         """
