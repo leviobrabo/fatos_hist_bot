22800be77e7e319de7fd0ee5969e8de65b56805f
Levi <115052769+leviobrabo@users.noreply.github.com>
2026-01-05 21:52:18 -0300

    Change scheduled time for historical images in channel

--- fatoshist/scheduled.py
+++ fatoshist/scheduled.py
@@ -97,7 +97,7 @@ def schedule_tasks(bot: TeleBot):
         schedule.every().day.at('15:00').do(lambda: hist_image_chat_job(bot))
 
         # Envio de Fotos históricas no canal
-        schedule.every().day.at('21:50').do(lambda: hist_channel_imgs(bot))
+        schedule.every().day.at('21:55').do(lambda: hist_channel_imgs(bot))
 
         # Envio de imagens historicas no canal de imagem
         schedule.every(8).hours.do(lambda: hist_channel_imgs_chn(bot))
